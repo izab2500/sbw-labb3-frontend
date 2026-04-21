b7003b0e742d87b72625a931dfbe59a3b96a951c
ziba <ziba2500@student.miun.se>
2026-04-21 19:17:02 +0200

    Add object representing a work experience

---
+++ src/js/modules/workexperienceObj.js
@@ -0,0 +1,10 @@
+/**
+ * Globalt objekt som används som referens för
+ * att skapa en giltig eller ogiltig arbetserfarenhet.
+ */
+export const workexperienceObj = {
+    companyname: "",
+    jobtitle: "",
+    location: "",
+    description: ""
+}
